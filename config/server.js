e1e223a4bb29e269c36c5b72df9debdf68e47e87
hayatonagasaki <h.nagasaki8562@gmail.com>
2024-01-11 13:59:21 +0900

    update docker

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://13.231.55.26'),
+  proxy: env.bool('IS_PROXIED', true),
   app: {
     keys: env.array('APP_KEYS'),
   },
