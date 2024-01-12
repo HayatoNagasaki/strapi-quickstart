193680093783ffb873dec2d1c91edc0ea956845b
hayatonagasaki <h.nagasaki8562@gmail.com>
2024-01-12 11:59:57 +0900

    fix docker things

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://13.231.55.26'),
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
   proxy: env.bool('IS_PROXIED', true),
   app: {
     keys: env.array('APP_KEYS'),
